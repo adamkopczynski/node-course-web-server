4cdec218d0cb24146206144321d2ce490d01a61b
Adam <akadamkopczynski@gmail.com>
2018-06-20 12:59:02 +0200

    add start script; add port variable;

--- server.js
+++ server.js
@@ -54,6 +54,8 @@ app.get('/bad', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is run on port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is run on port ${port}`)
 })
